19ec16a0f4026cf166ae58999fcc78709803db18
Chris Sevilleja <chris@scotch.io>
2019-02-04 19:59:27 -0800

    site nav done

---
+++ src/components/SiteNav.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="nav" class="navbar is-dark">
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <router-link to="/" class="navbar-item">Home</router-link>
+        <router-link to="/about" class="navbar-item">About</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
