09e1a73be6447c9346a4a587f91325250d25032c
Chris Sevilleja <chris@scotch.io>
2019-02-04 20:57:14 -0800

    starting user component

---
+++ src/components/User.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card">
+    <div class="card-image">User Image goes here</div>
+    <div class="card-content">
+      <h2>Username</h2>
+      <p>GitHub Followers: 54000</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <div v-for="user in users" :key="user.login">
-      <h2>{{ user.login }}</h2>
+  <div class="about section">
+    <div class="container">
+      <h1>This is an about page</h1>
+      <div class="columns">
+        <div class="column" v-for="user in users" :key="user.login">
+          <User/>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import User from "@/components/User";
+
 export default {
+  // register components
+  components: { User },
+
   // data for this component goes in here
   data() {
     return {
